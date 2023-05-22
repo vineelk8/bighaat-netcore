4fcc50fefbcbff86c83fc1feb0f9c5d97453c126
Vineel <vinilk8@gmail.com>
2023-05-22 12:32:02 +0530

    added react navigation

--- netcorebighaatv3/android/app/src/main/java/com/netcorebighaatv3/MainActivity.java
+++ netcorebighaatv3/android/app/src/main/java/com/netcorebighaatv3/MainActivity.java
@@ -3,6 +3,7 @@ package com.netcorebighaatv3;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "netcorebighaatv3";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
